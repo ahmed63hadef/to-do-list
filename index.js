c6f3f04a3a5ded3a38783081d85c925d60418d3e
ahmed63hadef <ahmedhadef63@gmail.com>
2023-04-02 06:18:34 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  list.innerHTML += `<li>${item.value}</li>`;
+  item.value = "";
+});
+
+list.addEventListener("click", (e) => {
+  if (e.target.classList.contains("checked")) {
+    e.target.remove();
+  } else {
+    e.target.classList.add("checked");
+  }
+});
+
+// e.target.remove();
